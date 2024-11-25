05c0611c7c0f13b9f0ec96d6c5efd2a00f4e1831
KalhariEkanayake <kalharichathumini@gmail.com>
2024-11-25 22:12:54 +0530

    Add user entity

--- servers/apps/users/src/users.service.ts
+++ servers/apps/users/src/users.service.ts
@@ -1,8 +1,40 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto, RegisterDto } from './dto/user.dto';
 
 @Injectable()
 export class UsersService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private readonly jwtService: JwtService,
+    // Private read only prisma
+    private readonly configService: ConfigService,
+  ) {}
+
+  // Register user service
+  async register(registerDto: RegisterDto) {
+    const { name, email, password } = registerDto;
+    const user = {
+      name,
+      email,
+      password
+    };
+    return user;
+  }
+
+  // Login service
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const user = {
+      email,
+      password
+    };
+    return user;
+  }
+
+  // Get all users service
+  async getUsers() {
+    const users = [];
+    return users;
   }
 }
